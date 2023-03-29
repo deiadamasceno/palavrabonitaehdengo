29b211ecc26883b91011bb07a22802c9e444b10e
deiadamasceno <129215352+deiadamasceno@users.noreply.github.com>
2023-03-29 20:51:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
 var btn = document.querySelector(".no");
 var position
+btn.addEventListener("click", function() {
+position ? (position = 0) : (position = 150);
+btn.style.transform = `translate(${position}px,0px)`;
+btn.style.transition = "all 0.3s ease";
+});
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    btn.addEventListener("touchstart", function() {
-        position ? (position = 0) : (position = 150);
-        btn.style.transform = `translate(${position}px,0px)`;
-        btn.style.transition = "all 0.3s ease";
-      });
-      
-      btn.addEventListener("touchend", function() {
-        position ? (position = 0) : (position = 150);
-        btn.style.transform = `translate(${position}px,0px)`;
-        btn.style.transition = "all 0.3s ease";
-      });
-  } else {
-    btn.addEventListener("mouseover", function() {
-        position ? (position = 0) : (position = 150);
-        btn.style.transform = `translate(${position}px,0px)`;
-        btn.style.transition = "all 0.3s ease";
-        });
-  }
+var btn = document.querySelector(".no");
+var position
+btn.addEventListener("mouseover", function() {
+position ? (position = 0) : (position = 150);
+btn.style.transform = `translate(${position}px,0px)`;
+btn.style.transition = "all 0.3s ease";
+});
 
 function tanks() {
-alert("Uhuuuuuuuu. TE ADORO MUUUITO! :)")
-}  
-
-function not() {
-alert("Você nem ta maluco de não aceitar, volte lá e clique no sim! :/")
-}  
+alert("tanks :)");
+}    
